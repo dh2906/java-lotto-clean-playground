5c2839720f0d1364911f46de83351171726721af
Donghun <2dh2@naver.com>
2025-02-15 15:02:33 +0900

    refactory. 번호 생성 시 매번 같은 리스트를 만드는 문제 해결

--- src/main/java/LottoNumberProvider.java
+++ src/main/java/LottoNumberProvider.java
@@ -4,13 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberProvider {
-    public List<Integer> getLottoNumber() {
-        List<Integer> lottoNumberList = new ArrayList<>();
-        List<Integer> lottoNumber = new ArrayList<>();
+    List<Integer> lottoNumberList = new ArrayList<>();
 
+    public LottoNumberProvider() {
         for (int i = 1; i <= 45; i++) {
             lottoNumberList.add(i);
         }
+    }
+
+    public List<Integer> getLottoNumber() {
+        List<Integer> lottoNumber;
 
         Collections.shuffle(lottoNumberList);
 
