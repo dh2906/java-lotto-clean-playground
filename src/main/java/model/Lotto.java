45d530920a5aec194a57efba7d576666a42908f7
Donghun <2dh2@naver.com>
2025-08-08 14:02:36 +0900

    feat. 생성자 및 유효성 검증 로직 추가

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,9 +1,23 @@
 package model;
 
+import java.util.Collections;
 import java.util.List;
+
+import exception.ErrorMessage;
 
 public class Lotto {
 
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+
+        this.numbers = Collections.unmodifiableList(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers == null || numbers.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_SIZE);
+        }
+    }
 }
