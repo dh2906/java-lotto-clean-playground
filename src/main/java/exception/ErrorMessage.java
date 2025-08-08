586943d919e0ed68a8809d287c60ddb7d3fea2c7
Donghun <2dh2@naver.com>
2025-08-08 14:24:07 +0900

    feat. 에러 메세지 클래스에서 관리

---
+++ src/main/java/exception/ErrorMessage.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class ErrorMessage {
+
+    public static final String INVALID_AMOUNT_VALUE = "금액은 1000원 단위의 양수여야 합니다.";
+    public static final String INVALID_NUMBER_FORMAT = "정수를 입력해야 합니다.";
+    public static final String INVALID_LOTTO_NUMBER_SIZE = "로또 번호는 6개여야 합니다.";
+}
