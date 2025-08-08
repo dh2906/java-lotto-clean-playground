c939fb2e40e547600ce8dc0522f721684d027486
Donghun <2dh2@naver.com>
2025-08-08 15:25:56 +0900

    feat. 2등 enum 추가

--- src/main/java/model/Rank.java
+++ src/main/java/model/Rank.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    THREE(3, 5_000),
-    FOUR(4, 50_000),
-    FIVE(5, 1_500_000),
-    SIX(6, 2_000_000_000);
+    THREE(3, false, 5_000),
+    FOUR(4, false, 50_000),
+    FIVE(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    SIX(6, false, 2_000_000_000);
 
     private final int matchCount;
+    private final boolean bonusMatched;
     private final int prize;
 
-    Rank(int matchCount, int prize) {
+    Rank(int matchCount, boolean bonusMatched, int prize) {
         this.matchCount = matchCount;
+        this.bonusMatched = bonusMatched;
         this.prize = prize;
     }
 
@@ -21,13 +24,17 @@ public enum Rank {
         return matchCount;
     }
 
+    public boolean isBonusMatched() {
+        return bonusMatched;
+    }
+
     public int getPrize() {
         return prize;
     }
 
-    public static Rank of(int matchCount) {
+    public static Rank of(int matchCount, boolean bonusMatched) {
         return Arrays.stream(Rank.values())
-            .filter(r -> r.getMatchCount() == matchCount)
+            .filter(r -> r.matchCount == matchCount && r.bonusMatched == bonusMatched)
             .findFirst()
             .orElse(null);
     }
