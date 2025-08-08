f3d43da8ce2d6539b607f398ccfb13c8d64abb05
Donghun <2dh2@naver.com>
2025-08-08 14:22:47 +0900

    feat. 로또 생성 구현

--- src/main/java/model/LottoTicket.java
+++ src/main/java/model/LottoTicket.java
@@ -1,8 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import util.LottoGenerator;
 
 public class LottoTicket {
 
     private List<Lotto> lottos;
+
+    public void generateLottos(LottoGenerator generator, int count) {
+        List<Lotto> temp = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            temp.add(generator.generate());
+        }
+
+        this.lottos = Collections.unmodifiableList(temp);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }
