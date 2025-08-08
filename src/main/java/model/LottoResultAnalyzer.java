b550c01a2c377f20613099ddec36c025945221a0
Donghun <2dh2@naver.com>
2025-08-08 15:08:15 +0900

    feat. 로또 등수 계산기 구현

---
+++ src/main/java/model/LottoResultAnalyzer.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResultAnalyzer {
+
+    private final Map<Rank, Integer> result = new EnumMap<>(Rank.class);
+
+    public LottoResultAnalyzer() {
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
+        }
+    }
+
+    public void analyze(LottoTicket lottoTicket, WinningNumbers winningNumbers) {
+        for (Lotto lotto : lottoTicket.getLottos()) {
+            int matchCount = (int) lotto.getNumbers()
+                .stream()
+                .filter(winningNumbers.getNumbers()::contains)
+                .count();
+
+            Rank rank = Rank.of(matchCount);
+
+            if (rank != null) {
+                result.put(rank, result.get(rank) + 1);
+            }
+        }
+    }
+
+    public Map<Rank, Integer> getResult() {
+        return result;
+    }
+
+    public double calculateProfitRate(int purchaseAmount) {
+        long totalWinning = result.entrySet().stream()
+            .mapToLong(entry -> entry.getKey().getPrize() * entry.getValue())
+            .sum();
+
+        return (double) totalWinning / purchaseAmount;
+    }
+}
