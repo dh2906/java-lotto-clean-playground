bdc77ee3bf69f4b6c9aca6439829c7ab4e9ce11f
Donghun <2dh2@naver.com>
2025-08-08 14:22:27 +0900

    feat. 로또 구매 결과 출력 클래스 생성

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,17 @@
+package view;
+
+import model.Lotto;
+import model.LottoTicket;
+
+public class OutputView {
+
+    public void printPurchaseCount(int count) {
+        System.out.printf("%d개를 구매했습니다. \n", count);
+    }
+
+    public void printLottoNumbers(LottoTicket lottoTicket) {
+        for (Lotto lotto : lottoTicket.getLottos()) {
+            System.out.println(lotto);
+        }
+    }
+}
