3905a961bd6c499db8c20b02247f665fc50cee16
Donghun <2dh2@naver.com>
2025-08-08 15:27:42 +0900

    feat. 보너스 번호 중복 에러 메시지 추가

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -6,5 +6,6 @@ public class ErrorMessage {
     public static final String INVALID_NUMBER_FORMAT = "정수를 입력해야 합니다.";
     public static final String INVALID_NUMBERS_SIZE = "로또 번호는 6개여야 합니다.";
     public static final String DUPLICATE_WINNING_NUMBER = "당첨 번호가 중복되었습니다.";
-    public static final String INVALID_WINNING_NUMBER_RANGE = "당첨 번호는 1~45 사이의 정수여야 합니다.";
+    public static final String INVALID_NUMBER_RANGE = "로또 번호는 1~45 사이의 정수여야 합니다.";
+    public static final String DUPLICATE_BONUS_NUMBER = "보너스 번호가 중복되었습니다.";
 }
