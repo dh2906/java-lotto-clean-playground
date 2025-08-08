d75c73e55c94512bff975eda3b252bcf549729ad
Donghun <2dh2@naver.com>
2025-08-08 16:21:43 +0900

    feat. 수동 구매 관련 에러 메시지 추가

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -8,4 +8,7 @@ public class ErrorMessage {
     public static final String DUPLICATE_WINNING_NUMBER = "당첨 번호가 중복되었습니다.";
     public static final String INVALID_NUMBER_RANGE = "로또 번호는 1~45 사이의 정수여야 합니다.";
     public static final String DUPLICATE_BONUS_NUMBER = "보너스 번호가 중복되었습니다.";
+    public static final String INVALID_MANUAL_COUNT_NEGATIVE = "수동 로또 개수는 0 이상이어야 합니다.";
+    public static final String INVALID_MANUAL_COUNT_EXCEED = "수동 로또 개수가 총 구매 가능한 로또 개수를 초과할 수 없습니다.";
+
 }
