8eed554ae9068838833164655b6a501940ed1666
Donghun <2dh2@naver.com>
2025-08-08 16:20:58 +0900

    feat. 수동 로또 번호 입력 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -24,5 +26,25 @@ public class InputView {
         System.out.println("보너스 볼을 입력해 주세요.");
 
         return sc.nextLine();
+    }
+
+    public String inputManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+
+        return sc.nextLine();
+    }
+
+    public List<String> inputManualLottoNumbers(int count) {
+        System.out.println();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        List<String> manualNumbers = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            String numbers = sc.nextLine();
+            manualNumbers.add(numbers);
+        }
+
+        return manualNumbers;
     }
 }
