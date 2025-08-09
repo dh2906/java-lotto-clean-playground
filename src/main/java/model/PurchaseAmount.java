74e73b6c8c0d6ec43bc0d78036d41661f3fa4952
Donghun <2dh2@naver.com>
2025-08-10 00:24:20 +0900

    refactor. 매직 넘버 상수 변수 사용

--- src/main/java/model/PurchaseAmount.java
+++ src/main/java/model/PurchaseAmount.java
@@ -20,7 +20,7 @@ public class PurchaseAmount {
     }
 
     private void validate(int amount) {
-        if (amount <= 0 || amount % 1000 != 0) {
+        if (amount <= 0 || amount % LOTTO_PRICE != 0) {
             throw new CustomException(ErrorMessage.INVALID_AMOUNT_VALUE);
         }
     }
