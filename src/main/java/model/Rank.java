a63b95bfc728d5a57338202db32378c50fb7d422
Donghun <2dh2@naver.com>
2025-08-10 00:40:47 +0900

    refactor. 로또 당첨 등수 이름 변경

--- src/main/java/model/Rank.java
+++ src/main/java/model/Rank.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    THREE(3, false, 5_000),
-    FOUR(4, false, 50_000),
-    FIVE(5, false, 1_500_000),
+    FIFTH(3, false, 5_000),
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
     SECOND(5, true, 30_000_000),
-    SIX(6, false, 2_000_000_000);
+    FIRST(6, false, 2_000_000_000);
 
     private final int matchCount;
     private final boolean bonusMatched;
